64e97e1a094ddd90d7f6dfa4ea8e372543151ab2
An Ba <ngwin.ab@gmail.com>
2023-03-23 10:31:53 -0400

    Dragonnnn

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import cowsay
 
-<<<<<<< HEAD
-cowsay.cow("Moooooooooo! Holy Cow!")
-=======
 cowsay.cow("Chào buổi sáng An Bá!")
->>>>>>> 93c25886c2645482bfc524f572e827d78a4c8d42
+
+cowsay.dragon("HI!!!!")
