a97354a6e1051177e7882c96750588aac4d1bd6c
Angela <echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"')  /Users/an5073/.zprofile>
2023-03-23 10:45:51 -0400

    Beautiful story of cow and dragon

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import cowsay
 
 cowsay.cow("Chào buổi sáng An Bá!")
-<<<<<<< HEAD
 '''
           .-~~~-.            
   .- ~ ~-(       )_ _
@@ -11,8 +10,4 @@ cowsay.cow("Chào buổi sáng An Bá!")
    ~- . _____________ . -~
 '''
 cowsay.dragon("Do not go where the path may lead, go instead where there is no path and leave a trail.")
-cowsay.cow("Ok")
-=======
-
-cowsay.dragon("HI!!!!")
->>>>>>> 64e97e1a094ddd90d7f6dfa4ea8e372543151ab2
+cowsay.cow("Ok!")
